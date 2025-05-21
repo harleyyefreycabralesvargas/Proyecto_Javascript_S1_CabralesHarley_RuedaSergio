db278f5233b1b8812ccb45054ebbdd219156c0ad
harley <arleycabralesvargas@gmail.com>
2025-05-20 20:54:22 -0500

    feat: :fire: login part five and menu options user

--- scripts/scripts_login.js
+++ scripts/scripts_login.js
@@ -1,23 +1,88 @@
 
 
-import datos from '../data/data.json' with { type: 'json' };
+function getusuarios(tomar_correo1, tomar_contraseña1){
+    let link = `https://682b2bf7ab2b5004cb396ecf.mockapi.io/usuarios`;
+    axios.get(link)
+    .then((response) => {
+        if (response.status == 200) {
+            console.log(response);
+            console.log(response["data"][0]);
+            let daticos=response.data;
+            let i=0
+
+let correo_userc = "";
+
+for(i=0;i<daticos.length;i++){
+
+    correo_userc = daticos[i].correo;
+
+    let a="";
+    if(correo_userc ==tomar_correo1){
+        a=i
+        let usuarios_user =daticos[a].tipo_usuario
+        let correo_user = daticos[a].correo
+        let contrasena_user = daticos[a].contrasena
+        console.log(usuarios_user);
+        console.log(correo_user);
+        console.log(contrasena_user)
+        if(tomar_correo1==correo_user && usuarios_user=="user"){
+            console.log("tomaron los datos para verificar user")
+            let link_incio=document.getElementById("redirecciones")
+                link_incio.innerHTML=`  
+            <div id="redirecciones">
+            <button class="boton" type="button" id="entrar"><a class="text_boton" href="./menu_opciones_user.html"><p class="text_boton">Entrar user</p></a></button>
+            <p class="text_Register">Si no tienes cuenta, crea una ||</p>
+            <a href="./index_register.html">
+                <p class="create_account">Crear cuenta</p>
+            </a>
+            </div>`
+            }
+        else if(tomar_correo1==correo_user && usuarios_user=="admin"){
+            if(tomar_contraseña1==contrasena_user){
+                let link_incio=document.getElementById("redirecciones")
+                console.log("probar si es admin")
+                link_incio.innerHTML=`  
+                
+                    <div id="redirecciones">
+                    <button class="boton" type="button" id="entrar"><a class="text_boton" href="./menu_opciones_admin.html"><p class="text_boton">Entrar admin</p></a></button>
+                    <p class="text_Register">Si no tienes cuenta, crea una ||</p>
+                    <a href="./index_register.html">
+                        <p class="create_account">Crear cuenta</p>
+                    </a>
+                    </div>`
+            }
+        }
+        else{
+            console.log("no existe")
+            let link_incio=document.getElementById("redirecciones")
+            link_incio.innerHTML=` 
+            
+            <div id="redirecciones">
+            <h1>correo o contraseña incorrectos</h1>
+            <button class="boton" type="button" id="entrar"><a class="text_boton" href=""><p class="text_boton">no Entrar</p></a></button>
+            <p class="text_Register">Si no tienes cuenta, crea una ||</p>
+            <a href="./index_register.html">
+                <p class="create_account">Crear cuenta</p>
+            </a>
+            </div>`
+        }
+    }
 
 
 
-let i=0
-let usuarios_user ="";
-let correo_user = "";
-let contrasena_user = "";
-for(i=0;i<datos.usuarios.length;i++){
-usuarios_user =datos.usuarios[i].usuario
-correo_user = datos.usuarios[i].correo
-contrasena_user = datos.usuarios[i].contraseña
 
 
-console.log(usuarios_user);
-console.log(correo_user);
-console.log(contrasena_user)
-}
+        
+            
+        
+    }
+    }
+})};
+
+
+
+
+
 
 
 
@@ -26,10 +91,10 @@ console.log(contrasena_user)
 // let administradores_admin ="";
 // let correo_admin = "";
 // let contrasena_admin = "";
-// for(o=0;o<datos.administradores.length;o++){
-// administradores_admin =datos.administradores[o].usuario
-// correo_admin = datos.administradores[o].correo
-// contrasena_admin = datos.administradores[o].contraseña
+// for(o=0;o<daticos.administradores.length;o++){
+// administradores_admin =daticos.administradores[o].usuario
+// correo_admin = daticos.administradores[o].correo
+// contrasena_admin = daticos.administradores[o].contraseña
 // 
 // 
 // console.log(administradores_admin);
@@ -44,69 +109,5 @@ document.getElementById("contraseña").addEventListener("input",function(){
         let tomar_contraseña1 = document.getElementById("contraseña").value
         console.log(tomar_contraseña1)
         console.log(tomar_correo1)
-
-let i=0
-let usuarios_user ="";
-let correo_user = "";
-let contrasena_user = "";
-for(i=0;i<datos.usuarios.length;i++){
-usuarios_user =datos.usuarios[i].tipo_usuario
-correo_user = datos.usuarios[i].correo
-contrasena_user = datos.usuarios[i].contraseña
-
-
-console.log(usuarios_user);
-console.log(correo_user);
-console.log(contrasena_user)
-
-
-        if(tomar_correo1==correo_user && usuarios_user=="usuario"){
-            console.log("xd")
-            if(tomar_contraseña1==contrasena_user){
-                console.log("tttt")
-                let link_incio=document.getElementById("redirecciones")
-                console.log(link_incio)
-                link_incio.innerHTML=`  
-            <div id="redirecciones">
-            <button class="boton" type="button" id="entrar"><a class="text_boton" href="./menu_opciones_user.html"><p class="text_boton">Entrar</p></a></button>
-            <p class="text_Register">Si no tienes cuenta, crea una ||</p>
-            <a href="./index_register.html">
-                <p class="create_account">Crear cuenta</p>
-            </a>
-            </div>`
-            }
-            }
-            else if(tomar_correo1==correo_user && usuarios_user=="admin"){
-                if(tomar_contraseña1==contrasena_user){
-                let link_incio=document.getElementById("redirecciones")
-                console.log(link_incio)
-                link_incio.innerHTML=`  
-            <div id="redirecciones">
-            <button class="boton" type="button" id="entrar"><a class="text_boton" href="./menu_opciones_admin.html"><p class="text_boton">Entrar</p></a></button>
-            <p class="text_Register">Si no tienes cuenta, crea una ||</p>
-            <a href="./index_register.html">
-                <p class="create_account">Crear cuenta</p>
-            </a>
-            </div>`
-                }
-            }
-            
-            else{
-                console.log("ssss")
-                let link_incio=document.getElementById("redirecciones")
-                link_incio.innerHTML=` 
-                
-                <div id="redirecciones">
-                <h1>correo o contraseña incorrectos</h1>
-                <button class="boton" type="button" id="entrar"><a class="text_boton" href="./index.html"><p class="text_boton">Entrar</p></a></button>
-                <p class="text_Register">Si no tienes cuenta, crea una ||</p>
-                <a href="./index_register.html">
-                    <p class="create_account">Crear cuenta</p>
-                </a>
-                </div>`
-            }
-            
-        
-    }
-    }
-});
+        getusuarios(tomar_correo1, tomar_contraseña1)
+    }})
--- scripts/scripts_register.js
+++ scripts/scripts_register.js
@@ -1,95 +1,46 @@
-function gethomo() {
+function getusuarios(usuario,correo,tipo_usuario,contrasena) {
     let link = `https://682b2bf7ab2b5004cb396ecf.mockapi.io/usuarios`;
     axios.get(link)
     .then((response) => {
         if (response.status == 200) {
             console.log(response);
             console.log(response["data"][0]);
+            let confirm_user = ""
+            let confirm_correo = ""
+            for(let i=0;i<response.data.length;i++){
+                if (response.data[i]["usuario"]==usuario || response.data[i]["correo"]==correo){
+                    confirm_user = response.data[i]["usuario"]
+                    confirm_correo = response.data[i]["correo"]
+                }
+            }
+            if (confirm_user=="" && confirm_correo==""){
+                console.log(confirm_correo)
+                console.log(confirm_user)
+                axios.post(link, {
+                    "usuario": usuario,
+                    "correo": correo,
+                    "tipo_usuario": tipo_usuario,
+                    "contrasena": contrasena,
+                })
+                .then((response) => {
+                    console.log(response)
+                    document.getElementById("formulariologin").reset();
+                })
+            } else{
+                alert("Correo o usuario repetido")
+            }
+        
         }
-    });
-}
-gethomo();
-function postuser(usuario,correo,contrasena,tipo_usuario) {
-    let link = `https://682b2bf7ab2b5004cb396ecf.mockapi.io/usuarios`;
-    axios.post(link, {
-        "usuario": usuario,
-        "correo": correo,
-        "tipo_usuario": tipo_usuario,
-        "contrasena": contrasena,
-    })
-    .then((response) => {
-        console.log(response)
-        document.getElementById("formulariologin").reset();
-    })
-}
+        })
+};
 
-document.getElementById("formulariologin").addEventListener("submit", function(e) {
+
+document.getElementById("formulariologin").addEventListener("submit",async function(e) {
     e.preventDefault();
     console.log("xd")
     let usuario = document.getElementById("usuario").value;
     let correo = document.getElementById("correo").value;
     let contrasena = document.getElementById("contraseña").value;
-    let tipo_usuario = "usuario";
-    postuser(usuario,correo,contrasena,tipo_usuario);
+    let tipo_usuario = "user";
+    getusuarios(usuario,correo,tipo_usuario,contrasena)
 })
-
-
-// function posthomo(nombre_personaje, nombre_actor, edad, ubicacion, poster, fecha_de_nacimiento, compania,iterador) {
-//     let link = `https://682b2bf7ab2b5004cb396ecf.mockapi.io/usuarios`;
-//     axios.post(link, {
-//         "Name": nombre_personaje,
-//         "Actor": nombre_actor,
-//         "Age": edad,
-//         "Location": ubicacion,
-//         "Poster":poster,
-//         "Date":fecha_de_nacimiento,
-//         "Company":compania,
-//         "Skin":[iterador]
-        
-//     })
-//     .then((response) => {
-//         console.log(response)
-//         document.getElementById("formularioHeroe").reset();
-//     })
-// }
-// document.getElementById("formularioHeroe").addEventListener("submit", function(e) {
-//     e.preventDefault();
-
-//     let nombre_personaje = document.getElementById("personaje").value;
-//     let nombre_actor = document.getElementById("actor").value;
-//     let edad = document.getElementById("edad").value;
-//     let ubicacion = document.getElementById("ubicacion").value;
-//     let poster = document.getElementById("poster").value;
-//     let fecha_de_nacimiento = document.getElementById("fecha").value;
-//     let compania = document.getElementById("productora").value;
-//     let count = 0;
-//     let iterador = {};
-
-//     for (let i = 1; document.getElementById(`numerito_${i}`) !== null; i++) {
-//         count++;
-//         let elemento = document.getElementById(`numerito_${i}`);
-
-//         iterador += {i:elemento.value}
-//     }
-
-//     posthomo(nombre_personaje, nombre_actor, edad, ubicacion, poster, fecha_de_nacimiento, compania, iterador);
-// });
-// let numerito = 0;
-// document.getElementById("boton_mas").addEventListener("click", function(e) {
-//     e.preventDefault();
-//     numerito++;
-
-//     let nuevo = document.getElementById("registro_trajes");
-
-//     let xd = `<br>
-//     <div class="traje_nuevo">
-//             <p>Nombre traje</p>
-//             <input id="numerito_${numerito}"type="text">
-//             <button type="button" class="eliminar">-</button>}
-//     </div>`;
-//     nuevo.insertAdjacentHTML("beforeend", xd);
-// });
-
-// function posthomo2(name_skins){
-//     let skinzzz = name_skins
-// }
